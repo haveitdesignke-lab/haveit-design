ffee7363f0a21cf5783f67b728a5edf7a6f32f45
Haveit Design <haveitdesignke-lab@users.noreply.github.com>
2026-09-20 22:05:59 +0300

    professional addons merged

--- Program.cs
+++ Program.cs
@@ -1,31 +1,40 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
 builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
 app.UseAuthorization();
 
-// --- FORCE robots.txt and sitemap.xml to work even if wwwroot fails ---
 app.MapGet("/robots.txt", () => "User-agent: *\nAllow: /\nSitemap: https://haveit-design-ke-e3g4gverbndkfjaz.southafricanorth-01.azurewebsites.net/sitemap.xml");
 app.MapGet("/sitemap.xml", () => Results.Content(
 @"<?xml version=""1.0"" encoding=""UTF-8""?>
 <urlset xmlns=""http://www.sitemaps.org/schemas/sitemap/0.9"">
-  <url><loc>https://haveit-design-ke-e3g4gverbndkfjaz.southafricanorth-01.azurewebsites.net/</loc><changefreq>weekly</changefreq><priority>1.0</priority></url>
-  <url><loc>https://haveit-design-ke-e3g4gverbndkfjaz.southafricanorth-01.azurewebsites.net/Home/Privacy</loc><priority>0.3</priority></url>
-  <url><loc>https://haveit-design-ke-e3g4gverbndkfjaz.southafricanorth-01.azurewebsites.net/Home/Terms</loc><priority>0.3</priority></url>
+  <url>
+    <loc>https://haveit-design-ke-e3g4gverbndkfjaz.southafricanorth-01.azurewebsites.net/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
 </urlset>", "application/xml"));
-// --- END FIX ---
+
+app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 
 app.Run();
